88689f490fec03f0cb87007701255f1a7f668250
innocodes <oyebodeinnocent@gmail.com>
2023-09-29 12:07:24 +0100

    Added Winner and Next Player Display Info

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
-import Square from "./Square/Square";
+import Square from "../Square/Square";
+import "./Board.css";
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(""));
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   const handleClick = (i: number) => {
     if (squares[i] || calculateWinner(squares)) {
@@ -21,6 +30,7 @@ export default function Board() {
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
